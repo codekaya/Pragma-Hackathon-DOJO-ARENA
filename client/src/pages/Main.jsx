04ad793601d67de22e462124a6baec39b932b6fe
Emir Soyturk <emirsytrk@gmail.com>
2023-06-24 19:02:51 +0300

    css improvement main page

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,5 +1,9 @@
 import Layout from "../components/Layout";
 import Logo from "../assets/react.svg";
+import CivilRights from "/civil-rights.png";
+import Strategy from "/random.png";
+import SilverPot from "/silver-pot.png";
+import Web3 from "/web-3.png";
 import DojoArena from "/dojo_arena.png";
 
 function Intro() {
@@ -29,10 +33,10 @@ function Intro() {
 function JoinAndRegister() {
   return (
     <div className="flex flex-row items-center space-x-10 mt-16 w-full px-20">
-      <button className="bg-[#386A92] px-4 py-4 rounded-md w-full">
+      <button className="bg-[#386A92] px-4 py-4 border-2 border-[#628EAB] rounded-md w-full shadow-button_1">
         <span>Join A Room!</span>
       </button>
-      <button className="bg-[#02040A] px-4 py-4 rounded-md w-full">
+      <button className="bg-[#02040A] px-4 py-4 border-2 border-[#4FCDF2] rounded-md w-full">
         <span>Register Your NFT Collection</span>
       </button>
     </div>
@@ -45,40 +49,40 @@ function Highlights() {
       <h2 className="text-[24px]">Highlights</h2>
       <div className="grid grid-cols-4 justify-items-center mx-auto gap-5 mt-12">
         <div className="flex flex-col space-y-2 justify-start py-2 px-2">
-          <div className="bg-black border border-blue-600 px-12 py-4">
-            <img src={Logo} alt="logo" className="w-20 h-20" />
+          <div className="bg-black border border-[#4FCDF2] px-12 py-4">
+            <img src={CivilRights} alt="logo" className="w-20 h-20" />
           </div>
-          <h3>No Token Advantage</h3>
-          <p className="text-[11px]">
+          <h3 className="text-[15px] font-bold">No Token Advantage</h3>
+          <p className="text-[11px] font-normal">
             Game tokens are destined to be dumped. So, in Dojo Arena there is
             only ETH / DAI token in gameplay.
           </p>
         </div>
         <div className="flex flex-col space-y-2 justify-start py-2 px-2">
-          <div className="bg-black border border-blue-600 px-12 py-4">
-            <img src={Logo} alt="logo" className="w-20 h-20" />
+          <div className="bg-black border border-[#4FCDF2] px-12 py-4">
+            <img src={SilverPot} alt="logo" className="w-20 h-20" />
           </div>
-          <h3>Winners Win ETH</h3>
-          <p className="text-[11px]">
+          <h3 className="text-[15px] font-bold">Winners Win ETH</h3>
+          <p className="text-[11px] font-normal">
             In Dojo Arena you do not earn in-game token. You directly rewarded
             with $Eth. If you create a room you earn 1% total reward.
           </p>
         </div>
         <div className="flex flex-col space-y-2 justify-start py-2 px-2">
-          <div className="bg-black border border-blue-600 px-12 py-4">
-            <img src={Logo} alt="logo" className="w-20 h-20" />
+          <div className="bg-black border border-[#4FCDF2] px-12 py-4">
+            <img src={Strategy} alt="logo" className="w-20 h-20" />
           </div>
-          <h3>Chance & Strategy</h3>
-          <p className="text-[11px]">
+          <h3 className="text-[15px] font-bold">Chance & Strategy</h3>
+          <p className="text-[11px] font-normal">
             Rely on your chance & strategy not your tokens.
           </p>
         </div>
         <div className="flex flex-col space-y-2 justify-start py-2 px-2">
-          <div className="bg-black border border-blue-600 px-12 py-4">
-            <img src={Logo} alt="logo" className="w-20 h-20" />
+          <div className="bg-black border border-[#4FCDF2] px-12 py-4">
+            <img src={Web3} alt="logo" className="w-20 h-20" />
           </div>
-          <h3>For NFT Communities</h3>
-          <p className="text-[11px]">
+          <h3 className="text-[15px] font-bold">For NFT Communities</h3>
+          <p className="text-[11px] font-normal">
             Your Starknet NFT is more valuable now! Join a survivor game with
             your NFT community.
           </p>
@@ -93,17 +97,17 @@ function ToJoin() {
     <div className="flex flex-col text-center mt-20 space-y-4">
       <h2 className="text-[24px]">To Join A Game</h2>
       <div className="grid grid-cols-2 justify-items-center px-10 mx-auto gap-5">
-        <div className="flex flex-col spcae-y-1 border-2 border-black justify-start py-2 px-2">
-          <h3>Join a Room</h3>
-          <p>
+        <div className="flex flex-col spcae-y-1 border-2 border-black justify-start py-2 px-2 border-2 border-white shadow-border_1 rounded-lg ">
+          <h3 className="text-[16px] font-bold">Join a Room</h3>
+          <p className="text-[13px] font-light">
             If you are holding a Starknet NFT, you can join a room which
             prepared for your collection. Pay the entry fee, this entry fee
             cumulates for total reward.
           </p>
         </div>
-        <div className="flex flex-col spcae-y-1 border-2 border-black justify-start py-2 px-2">
-          <h3>Create a Room & Earn Ether</h3>
-          <p>
+        <div className="flex flex-col spcae-y-1 border-2 border-black justify-start py-2 px-2 border-2 border-white shadow-border_1 rounded-lg ">
+          <h3 className="text-[16px] font-bold">Create a Room & Earn Ether</h3>
+          <p className="text-[13px] font-light">
             Start your own room & set the rules. If your game starts you earn 1%
             commision of total rewards.
           </p>
@@ -117,13 +121,13 @@ function ToSurvive() {
   return (
     <div className="flex flex-col text-center mt-16">
       <h2 className="text-[24px]">To Survive</h2>
-      <p className="mt-2">
+      <p className="mt-2 text-[12px] w-2/3 mx-auto">
         In default game, each player is equal and starts with 2400 HP. Each
         epoch longs 6 hours and after the epoch ends you lose -600 health. Each
         epoch, you can do one of them.
       </p>
       <div className="grid grid-cols-3 justify-items-center mx-auto gap-5 mt-16">
-        <div className="flex flex-col space-y-2 border-2 border-black text-center justify-start py-2 px-2">
+        <div className="flex flex-col space-y-2 border-2 border-white shadow-border_1 text-center justify-start py-2 px-2">
           <div className="bg-[#D8D8D8] min-w-[100px] min-h-[100px] aspect-square" />
           <h3 className="text-[16px]">Hunting</h3>
           <h4 className="text-[13px]">Increase Your Health</h4>
@@ -132,7 +136,7 @@ function ToSurvive() {
             kill yourself in hunting.
           </p>
         </div>
-        <div className="flex flex-col space-y-2 border-2 border-black text-center justify-start py-2 px-2">
+        <div className="flex flex-col space-y-2 border-2 border-white shadow-border_1 text-center justify-start py-2 px-2">
           <div className="bg-[#D8D8D8] min-w-[100px] min-h-[100px] aspect-square" />
           <h3 className="text-[16px]">ATTACKING</h3>
           <h4 className="text-[13px]">Kill Other Champions</h4>
@@ -141,7 +145,7 @@ function ToSurvive() {
             Also, you can kill yourself when you attacking.
           </p>
         </div>
-        <div className="flex flex-col space-y-2 border-2 border-black text-center justify-start py-2 px-2">
+        <div className="flex flex-col space-y-2 border-2 border-white shadow-border_1 text-center justify-start py-2 px-2">
           <div className="bg-[#D8D8D8] min-w-[100px] min-h-[100px] aspect-square" />
           <h3 className="text-[16px]">HIDING</h3>
           <h4 className="text-[13px]">Be Untouchable For Next Turn</h4>
@@ -159,7 +163,7 @@ function ToWin() {
   return (
     <div className="flex flex-col text-center mt-16">
       <h2 className="text-[24px]">To Win</h2>
-      <p>
+      <p className="text-[17px] w-2/3 mx-auto">
         Be the Last Survivor. Win the 97% of the entry fees. More mods coming
         soon.
       </p>
@@ -171,7 +175,7 @@ function Rules() {
   return (
     <div className="flex flex-col text-center mt-20">
       <h2 className="text-[24px]">Rules: </h2>
-      <ul className="">
+      <ul className="text-[17px]">
         <li>Each turn you have a one move to play.</li>
         <li>Each turn you lose HP since starving.</li>
         <li>If you receive attack when you are hidding, it does not effect.</li>
@@ -181,38 +185,38 @@ function Rules() {
           game.
         </li>
       </ul>
-      <div className="grid grid-cols-2 justify-items-center mx-auto gap-5 w-2/3 mt-20">
-        <div className="flex flex-col justify-left border-2 border-white rounded-md">
-          <h3> P2E </h3>
-          <ul className="space-y-8">
+      <div className="grid grid-cols-2 justify-items-center mx-auto gap-5 px-40 mt-20">
+        <div className="flex flex-col justify-left border-2 border-white shadow-border_1 rounded-md py-4">
+          <h3 className="text-[24px] font-bold"> P2E </h3>
+          <ul className="flex flex-col juftify-between space-y-8 text-[12px] font-bold">
             <li className="relative">
               <span>Provides in-game advantage</span>
-              <span className="absolute top-0 -left-[9rem]">Token Holding</span>
+              <span className="absolute top-0 -left-[7rem]">Token Holding</span>
             </li>
             <li className="relative">
               <span>Early Comers</span>
-              <span className="absolute top-0 -left-[11.6rem]">
+              <span className="absolute top-0 -left-[9.1rem]">
                 Who has advantage
               </span>
             </li>
             <li> Financial Power has significant advantage on gameplay</li>
             <li className="relative">
               <span>Winner earns in-game token (to sell)</span>
-              <span className="absolute top-0 -left-[9rem]">Winner Takes</span>
+              <span className="absolute top-0 -left-[7rem]">Winner Takes</span>
             </li>
             <li className="relative">
               <span>P2E’s are Complex</span>
-              <span className="absolute top-0 -left-[8.1rem]">Complexity</span>
+              <span className="absolute top-0 -left-[6.4rem]">Complexity</span>
             </li>
             <li className="relative">
               <span>aligned with earning money</span>
-              <span className="absolute top-0 -left-[8rem]">Philosophy</span>
+              <span className="absolute top-0 -left-[6.5rem]">Philosophy</span>
             </li>
           </ul>
         </div>
-        <div className="flex flex-col justify-left border-2 border-green-400 rounded-md">
-          <h3> Dojo Arena </h3>
-          <ul className="space-y-8">
+        <div className="flex flex-col justify-left border-2 border-green-400 shadow-border_1 rounded-md py-4">
+          <h3 className="text-[24px] font-bold"> Dojo Arena </h3>
+          <ul className="space-y-8 text-[12px] font-bold">
             <li> no in-game advantage </li>
             <li> Best Strategy Makers </li>
             <li> Best Strategy has significant advantage on gameplay </li>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
     maxWidth: {
       dojo: "1000px",
     },
+    boxShadow: {
+      button_1: "0px 0px 14px 4px rgba(0,67,152, 0.638822)",
+      border_1:
+        "0px 0px 4px 1px rgba(97,255,252, 1) inset, 0px 0px 4px 1px rgba(40,211,255, 0.778382)",
+    },
   },
   plugins: [],
 };
